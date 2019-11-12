954d1e9d50503bddfdf1c3281cc7fcf842106158
chenqiushi <chenqiushi@tosunk.cn>
2019-11-12 22:06:04 +0800

    小程序
    Change-Id: Iefbfc32f7566917fc435e9c1a50fa09bbd305601
    Signed-off-by: chenqiushi <chenqiushi@tosunk.cn>

---
+++ my-shop-wx-mall/pages/ucenter/system/system.js
@@ -0,0 +1,75 @@
+// pages/ucenter/system.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    // 获取设备信息
+    wx.getSystemInfo({
+      success(res) {
+        // 异步将数据更新到视图层
+        that.setData({
+          systemInfo: res
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
